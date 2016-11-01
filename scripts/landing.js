32bd2332a63b12e6dd56fc27fcc33b3643a23382
Sergey Gorbatyuk <rrsg123@gmail.com>
2016-11-01 11:34:15 -0600

    finish 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
   
-	var animatePoints = function(points) {
-        
+var animatePoints = function(points) {
+	var revealPoint = function(index) {        
 	for(var i=0; i < points.length; i++) {
-		points[i].style.opacity = 1;
-		points[i].style.transform = "scaleX(1) translateY(0)";
-		points[i].style.msTransform = "scaleX(1) translateY(0)";
-		points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 	};		
 
-	revealFirstPoint();
-	revealSecondPoint();
-	revealThirdPoint();
+	for {var i = 0; i < points.lengthl i++) {
+		revealPoint(i);
+	}
 																				     
 };
 
@@ -20,11 +20,9 @@ window.onload = function() {
 		animatePoints(pointsArray);
  	}
 	
-	var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
+	
 	window.addEventListener('scroll', function(event) {
-		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+		if (pointsArray[0].getBoundingClientRect90.top <= 500) {
 			animatePoints(pointsArray);
 	}
 
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(points,callback) {
+for (i=0;i<points.length;i++) {
+return callback(points[i]); 
+}
+}
